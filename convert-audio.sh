dbfd4c03de4dcb5ee7b38697b278569883b7af7f
Adonis Gaitatzis <backupbrain@gmail.com>
2022-09-17 00:03:32 +0200

    tweaks

--- convert-audio.sh
+++ convert-audio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Convert 24k mp3 to 16x wav/raw
-for i in `ls src/*.ts`;
-    do ffmpeg -i $i -acodec pcm_s16le -ac 1 -ar 16000 "${i%.*}.wav";
+for i in `find data/ -name \*.mp3`;
+    do ffmpeg -i "${i}" -acodec pcm_s16le -ac 1 -ar 16000 "${i%.*}.wav";
 done
--- src/test.py
+++ src/test.py
@@ -1,4 +1,6 @@
 import os
+from os import listdir
+from os.path import isfile, join
 
 from pocketsphinx import Pocketsphinx, get_data_path, get_model_path
 from pydub import AudioSegment
@@ -17,16 +19,23 @@ config = {
 ps = Pocketsphinx(**config)
 
 
-sound = AudioSegment.from_mp3("data/sample.mp3")
+def get_sound_files(folder):
+    return [f for f in listdir(folder) if isfile(join(folder, f)) and f.endswith(".wav")]
 
-sound.export("data/sample.raw", format="wav", bitrate="16k")
 
-with open("data/sample.raw", 'wb') as file:
-    file.write(sound._data)
-    file.close()
+# sound = AudioSegment.from_mp3("data/sample.mp3")
+
+# sound.export("data/sample.raw", format="wav", bitrate="16k")
+
+# with open("data/sample.raw", 'wb') as file:
+#     file.write(sound._data)
+#     file.close()
+
+files = get_sound_files("data/")
+file = files[0]
 
 ps.decode(
-    audio_file=os.path.join(data_path, 'sample.wav'),
+    audio_file=os.path.join(data_path, file),
     buffer_size=2048,
     no_search=False,
     full_utt=False
